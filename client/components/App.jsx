0849dc3652c13d56b07c2782c009ee9ac0fd6fe3
Den50 <vip.shenyagin@mail.ru>
2017-10-28 21:12:28 +0300

    add client selection

---
+++ client/components/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = React.createClass({
+	render (){
+		return (
+		  <div className="container">
+		  	<h1>Its App</h1>
+		  </div>
+		);
+	}
+});
+export default App;
